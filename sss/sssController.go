26b958455b23ffafde2a06e6f0bf0d20db64839c
enrico.viola <enrico.viola@accenture.com>
2025-10-22 21:42:05 +0200

    starting to shape things out

--- sss/sssController.go
+++ sss/sssController.go
@@ -1,7 +1,12 @@
 package sss
 
-import(
-	
+import (
+	"fmt"
 )
 
-
+func MakeSSSRecipient(threshold int, publicKeys ...[]byte) ([]byte, error) {
+	if len(publicKeys) < threshold {
+		return nil, fmt.Errorf("the number of shared must be bigger than the threshold")
+	}
+	return nil, nil
+}
