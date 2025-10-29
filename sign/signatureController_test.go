5f5087a372b8ae32dfe9704422f60161a250d397
naus3a <naus3a@gmail.com>
2025-10-29 20:19:49 +0100

    tested signature verification

--- sign/signatureController_test.go
+++ sign/signatureController_test.go
@@ -19,6 +19,7 @@ func TestShareSigning(t *testing.T) {
 
 	signed := sign.SignShares(pubKey, privKey, fakeShares)
 
+	//test the share making
 	for i := 0; i < len(signed); i++ {
 		if signed[i].Share != base64.RawURLEncoding.EncodeToString(fakeShares[i]) {
 			t.Errorf("FAIL: corrupted share")
@@ -26,5 +27,30 @@ func TestShareSigning(t *testing.T) {
 		if signed[i].AuthKey != base64.RawURLEncoding.EncodeToString(pubKey) {
 			t.Errorf("FAIL: corrupted authentication key")
 		}
+	}
+
+	b, err := signed[0].VerifyShare(signed[1])
+	if err != nil {
+		t.Errorf("FAIL: verifying signature throws error: %v", err)
+	}
+	if !b {
+		t.Errorf("FAIL: verifying good signature failed")
+	}
+
+	wrongPub, wrongPriv, _ := sign.MakeSigningKeys()
+	wrongShares := sign.SignShares(wrongPub, wrongPriv, fakeShares)
+	b, _ = signed[0].VerifyShare(wrongShares[0])
+	if b {
+		t.Errorf("FAIL: failed to identify different authentication key")
+	}
+
+	tampered := &sign.ArmoredShare{
+		Share:     "badstuffhere",
+		Signature: signed[1].Signature,
+		AuthKey:   signed[1].AuthKey,
+	}
+	b, _ = signed[0].VerifyShare(tampered)
+	if b {
+		t.Errorf("FAIL: failed to identify tampered share")
 	}
 }
