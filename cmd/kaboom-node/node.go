f3baf4bcd28230e7f3d480d3f540f3b3b3f5e450
naus3a <naus3a@gmail.com>
2025-11-13 18:28:36 +0100

    rebuilding the node piece by piece

--- cmd/kaboom-node/node.go
+++ cmd/kaboom-node/node.go
@@ -1,21 +1,120 @@
 package main
 
 import (
-	//"fmt"
 	"context"
+	"fmt"
 	"github.com/naus3a/kaboom/cmd"
 	"github.com/naus3a/kaboom/remote"
+
+	"bufio"
+	"os"
+	"sync"
+
+	//"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 )
 
-func main(){
+var topicNameFlag string
+
+func main() {
+	topicNameFlag = "cippa"
+
 	ctx := context.Background()
-	comms, err := remote.NewPubSubComms("cippa", ctx)
+
+	comms, err := remote.NewPubSubComms(topicNameFlag, ctx)
 	cmd.ReportErrorAndExit(err)
-	go comms.DiscoverPeers()
-	
+
+	go discoverPeers(comms.TheCtx, comms.TheHost)
+
+	go streamConsoleTo(comms.TheCtx, comms.Topic)
+
 	err = comms.Listen()
 	cmd.ReportErrorAndExit(err)
-	cmd.ColorPrintln("Started Listening", cmd.Green)
 
+	printMessagesFrom(comms.TheCtx, comms.Sub)
+}
 
+func initDHT(ctx context.Context, h host.Host) *dht.IpfsDHT {
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	kademliaDHT, err := dht.New(ctx, h)
+	if err != nil {
+		panic(err)
+	}
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+	var wg sync.WaitGroup
+	for _, peerAddr := range dht.DefaultBootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := h.Connect(ctx, *peerinfo); err != nil {
+				//fmt.Println("Bootstrap warning:", err)
+			}
+		}()
+	}
+	wg.Wait()
+
+	return kademliaDHT
+}
+
+func discoverPeers(ctx context.Context, h host.Host) {
+	kademliaDHT := initDHT(ctx, h)
+	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
+	dutil.Advertise(ctx, routingDiscovery, topicNameFlag)
+
+	// Look for others who have announced and attempt to connect to them
+	anyConnected := false
+	for !anyConnected {
+		fmt.Println("Searching for peers...")
+		peerChan, err := routingDiscovery.FindPeers(ctx, topicNameFlag)
+		if err != nil {
+			panic(err)
+		}
+		for peer := range peerChan {
+			if peer.ID == h.ID() {
+				continue // No self connection
+			}
+			err := h.Connect(ctx, peer)
+			if err != nil {
+				//fmt.Printf("Failed connecting to %s, error: %s\n", peer.ID, err)
+			} else {
+				fmt.Println("Connected to:", peer.ID)
+				anyConnected = true
+			}
+		}
+	}
+	fmt.Println("Peer discovery complete")
+}
+
+func streamConsoleTo(ctx context.Context, topic *pubsub.Topic) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		if err := topic.Publish(ctx, []byte(s)); err != nil {
+			fmt.Println("### Publish error:", err)
+		}
+	}
+}
+
+func printMessagesFrom(ctx context.Context, sub *pubsub.Subscription) {
+	for {
+		m, err := sub.Next(ctx)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(m.ReceivedFrom, ": ", string(m.Message.Data))
+	}
 }
--- remote/pubSub.go
+++ remote/pubSub.go
@@ -1,151 +1,147 @@
 package remote
 
-import(
-	"fmt"
+import (
 	"context"
-	"sync"
+	// "fmt"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/peer"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
-	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	"github.com/naus3a/kaboom/cmd"
+	"github.com/libp2p/go-libp2p/core/host"
+	// "github.com/libp2p/go-libp2p/core/peer"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	// dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
+	// "github.com/naus3a/kaboom/cmd"
+	// "sync"
 )
 
 type PubSubComms struct {
-	chanName string
-	theCtx context.Context
-	theHost host.Host
-	pubSub *pubsub.PubSub
-	topic *pubsub.Topic
-	sub *pubsub.Subscription
-	theDht *dht.IpfsDHT
-	route *drouting.RoutingDiscovery
+	chanName          string
+	TheCtx            context.Context
+	TheHost           host.Host
+	pubSub            *pubsub.PubSub
+	Topic             *pubsub.Topic
+	Sub               *pubsub.Subscription
+	theDht            *dht.IpfsDHT
+	route             *drouting.RoutingDiscovery
 	hasConnectedPeers bool
 
 	OnPeerConnected func()
 }
 
-func NewPubSubComms(channelName string, ctx context.Context)(*PubSubComms, error){
-	h, err := makeHost()
-	if err!= nil{
-		return nil, err
-	}
-	cmd.ColorPrintln("Host ready.", cmd.Green)
-	kDht, err := initDht(ctx, h)
-	if err!=nil{
-		return nil, err
-	}
-	cmd.ColorPrintln("DHT ready.", cmd.Green)
-
-	routeDiscovery := drouting.NewRoutingDiscovery(kDht)
-	dutil.Advertise(ctx, routeDiscovery, channelName)
-	cmd.ColorPrintln("Routing ready.", cmd.Green)
-
-	ps, err := pubsub.NewGossipSub(ctx, h)
-	if err != nil{
-		return nil, err
-	}
-	t, err := ps.Join(channelName)
-	if err != nil{
-		return nil, err
-	}
-	cmd.ColorPrintln("Channel ready.", cmd.Green)
-
-	return &PubSubComms{
-		chanName: channelName,
-		theCtx: ctx,
-		theHost: h,
-		pubSub: ps,
-		topic: t,
-		sub: nil,
-		theDht: kDht,
-		route: routeDiscovery,
-		hasConnectedPeers: false,
-		OnPeerConnected: nil,
-
-	}, nil
-} 
-
-func (c *PubSubComms) DiscoverPeers(){
-	if c.theCtx==nil || c.theHost==nil || c.theDht==nil || c.route==nil{
-		cmd.ReportErrorAndExit(fmt.Errorf("PubSubComms is initialized"))
-	}
-	if c.hasConnectedPeers{
-		cmd.ColorPrintln("Peers already connected.", cmd.Yellow)
-		return
-	}
-	for !c.hasConnectedPeers{
-		cmd.ColorPrintln("Peer Discovery started", cmd.Green)
-		peerChan, err := c.route.FindPeers(c.theCtx, c.chanName)
-		if err !=nil{
-			cmd.ReportErrorAndExit(err)
-		}
-		for peer := range peerChan{
-			if peer.ID == c.theHost.ID(){
-				continue
-			}
-			err := c.theHost.Connect(c.theCtx, peer)
-			if err==nil{
-				txt := fmt.Sprintf("Connected to %s", peer.ID)
-				cmd.ColorPrintln(txt, cmd.Green)
-				if c.OnPeerConnected != nil{
-					c.OnPeerConnected()
-				}
-				c.hasConnectedPeers=true
-			}
-		}
-	}
-	cmd.ColorPrintln("Discovery complete", cmd.Green)
-}
-
-func (c *PubSubComms) Send(data []byte) error{
-	if !c.hasConnectedPeers {
-		return fmt.Errorf("Caannot send data, because not connected to any peer")
-	}
-	return c.topic.Publish(c.theCtx, data)
-}
-
-func (c *PubSubComms) Listen()error{
-	if c.topic == nil{
-		return fmt.Errorf("Comms not initializwd")
-	}
+func NewPubSubComms(channelName string, ctx context.Context) (*PubSubComms, error) {
+	c := new(PubSubComms)
 	var err error
-	c.sub, err = c.topic.Subscribe()
+	c.TheCtx = ctx
+	c.TheHost, err = libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.InitPubSub()
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *PubSubComms) InitPubSub() error {
+	var err error
+	c.pubSub, err = pubsub.NewGossipSub(c.TheCtx, c.TheHost)
+	if err != nil {
+		return err
+	}
+	c.Topic, err = c.pubSub.Join(c.chanName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *PubSubComms) Listen() error {
+	var err error
+	c.Sub, err = c.Topic.Subscribe()
 	return err
-
 }
 
-func makeHost()(host.Host, error){
-	return libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+/*
+func NewPubSubComms(channelName string, ctx context.Context) (*PubSubComms, error) {
+	comms := new(PubSubComms)
+	var err error
+	comms.chanName = channelName
+	comms.theCtx = ctx
+	comms.theHost, err = libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+	if err != nil {
+		return nil, err
+	}
+	go discoverPeers(comms.theCtx, comms.theHost, comms.chanName)
+	comms.pubSub, err = pubsub.NewGossipSub(comms.theCtx, comms.theHost)
+	if err != nil {
+		return nil, err
+	}
+	comms.topic, err = comms.pubSub.Join(comms.chanName)
+	if err != nil {
+		return nil, err
+	}
+
+	return comms, nil
 }
 
-func initDht(ctx context.Context, h host.Host)(*dht.IpfsDHT, error){
-	kDht, err := dht.New(ctx, h)
-	if err!=nil{
-		return nil, err
+func initDHT(ctx context.Context, h host.Host) *dht.IpfsDHT {
+	// Start a DHT, for use in peer discovery. We can't just make a new DHT
+	// client because we want each peer to maintain its own local copy of the
+	// DHT, so that the bootstrapping node of the DHT can go down without
+	// inhibiting future peer discovery.
+	kademliaDHT, err := dht.New(ctx, h)
+	if err != nil {
+		panic(err)
 	}
-	err = kDht.Bootstrap(ctx)
-	if err !=nil{
-		return nil, err
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
 	}
-
 	var wg sync.WaitGroup
-	for _, peerAddr := range dht.DefaultBootstrapPeers{
-		peerInfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+	for _, peerAddr := range dht.DefaultBootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
 		wg.Add(1)
-		go func(){
+		go func() {
 			defer wg.Done()
-			if err := h.Connect(ctx, *peerInfo); err != nil {
+			if err := h.Connect(ctx, *peerinfo); err != nil {
 				fmt.Println("Bootstrap warning:", err)
 			}
 		}()
 	}
 	wg.Wait()
-	
-	return kDht,nil
+
+	return kademliaDHT
 }
 
+func discoverPeers(ctx context.Context, h host.Host, chanName string) {
+	kademliaDHT := initDHT(ctx, h)
+	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
+	dutil.Advertise(ctx, routingDiscovery, chanName)
 
+	// Look for others who have announced and attempt to connect to them
+	anyConnected := false
+	for !anyConnected {
+		fmt.Println("Searching for peers...")
+		peerChan, err := routingDiscovery.FindPeers(ctx, chanName)
+		if err != nil {
+			panic(err)
+		}
+		for peer := range peerChan {
+			if peer.ID == h.ID() {
+				continue // No self connection
+			}
+			err := h.Connect(ctx, peer)
+			if err != nil {
+				fmt.Printf("Failed connecting to %s, error: %s\n", peer.ID, err)
+			} else {
+				fmt.Println("Connected to:", peer.ID)
+				anyConnected = true
+			}
+		}
+	}
+	fmt.Println("Peer discovery complete")
+}
+*/
