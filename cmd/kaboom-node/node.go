ebf8471b520a3ad823c7b1453223b2c7b25c8a0a
naus3a <naus3a@gmail.com>
2025-11-13 19:19:47 +0100

    cleanup

--- cmd/kaboom-node/node.go
+++ cmd/kaboom-node/node.go
@@ -8,13 +8,8 @@ import (
 
 	"bufio"
 	"os"
-	"sync"
 
-	//"github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/peer"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 )
@@ -39,34 +34,6 @@ func main() {
 	cmd.ColorPrintln("Listening.", cmd.Green)
 
 	printMessagesFrom(comms.TheCtx, comms.Sub)
-}
-
-func initDHT(ctx context.Context, h host.Host) *dht.IpfsDHT {
-	// Start a DHT, for use in peer discovery. We can't just make a new DHT
-	// client because we want each peer to maintain its own local copy of the
-	// DHT, so that the bootstrapping node of the DHT can go down without
-	// inhibiting future peer discovery.
-	kademliaDHT, err := dht.New(ctx, h)
-	if err != nil {
-		panic(err)
-	}
-	if err = kademliaDHT.Bootstrap(ctx); err != nil {
-		panic(err)
-	}
-	var wg sync.WaitGroup
-	for _, peerAddr := range dht.DefaultBootstrapPeers {
-		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if err := h.Connect(ctx, *peerinfo); err != nil {
-				//fmt.Println("Bootstrap warning:", err)
-			}
-		}()
-	}
-	wg.Wait()
-
-	return kademliaDHT
 }
 
 func discoverPeers(c *remote.PubSubComms) {
