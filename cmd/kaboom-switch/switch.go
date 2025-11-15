5e27260bf07f4d4b329bf7929c232e1af49dc374
naus3a <naus3a@gmail.com>
2025-11-15 16:44:12 +0100

    switch the same as node

--- cmd/kaboom-switch/switch.go
+++ cmd/kaboom-switch/switch.go
@@ -1,26 +1,60 @@
 package main
 
-import(
-	"fmt"
+import (
 	"context"
+	"fmt"
 	"github.com/naus3a/kaboom/cmd"
 	"github.com/naus3a/kaboom/remote"
+
+	"bufio"
+	"os"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	//drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	//dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 )
 
-func main(){
+var topicNameFlag string
+
+func main() {
+	topicNameFlag = "cippa"
+
 	ctx := context.Background()
-	comms, err := remote.NewPubSubComms("cippa", ctx)
+
+	comms, err := remote.NewPubSubComms(topicNameFlag, ctx)
 	cmd.ReportErrorAndExit(err)
-	
-	comms.OnPeerConnected = func(){
-		fmt.Print(cmd.ColorString("Sending Message...", cmd.Yellow))
-		
-		err = comms.Send([]byte("cippa"))
-		if err!= nil{
-			fmt.Println("\r"+cmd.GetAnsiCode(cmd.ClearLine)+cmd.ColorString("Message sent.", cmd.Green))
-		}else{
-			fmt.Println("\r"+cmd.GetAnsiCode(cmd.ClearLine)+cmd.ColorString("Message failed.", cmd.Red))
+	cmd.ColorPrintln("Comms ready.", cmd.Green)
+
+	go comms.DiscoverPeers()
+
+	go streamConsoleTo(comms.TheCtx, comms.Topic)
+
+	err = comms.Listen()
+	cmd.ReportErrorAndExit(err)
+	cmd.ColorPrintln("Listening.", cmd.Green)
+
+	printMessagesFrom(comms.TheCtx, comms.Sub)
+}
+
+func streamConsoleTo(ctx context.Context, topic *pubsub.Topic) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		if err := topic.Publish(ctx, []byte(s)); err != nil {
+			fmt.Println("### Publish error:", err)
 		}
 	}
-	comms.DiscoverPeers()
+}
+
+func printMessagesFrom(ctx context.Context, sub *pubsub.Subscription) {
+	for {
+		m, err := sub.Next(ctx)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(m.ReceivedFrom, ": ", string(m.Message.Data))
+	}
 }
