abab40de130f37c904d403a6851153bd7b26c66a
naus3a <naus3a@gmail.com>
2025-12-14 15:35:54 +0100

    started adding cli to switch

--- cmd/kaboom-switch/switch.go
+++ cmd/kaboom-switch/switch.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 	"context"
 	"os"
 	"github.com/naus3a/kaboom/cmd"
@@ -8,7 +10,39 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
+const usage = `Usage:
+kaboom-switch -s signing.keys
+
+Options:
+	-h, --help	this help screen
+	-v, --version	prints version
+	-s, --sign	path to the signing key
+`
+
 func main() {
+	var hFlag bool
+	var vFlag bool
+	var sFlag string
+
+	cmd.InitCli(usage)
+	cmd.AddArg(&hFlag, false, "h", "help")
+	cmd.AddArg(&vFlag, false, "v", "version")
+	cmd.AddArg(&sFlag, "", "s", "sign")
+
+	flag.Parse()
+
+	if hFlag {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if vFlag {
+		fmt.Println(cmd.Version)
+		os.Exit(0)
+	}
+
+	
+
 	ctx := context.Background()
 
 	comms, err := remote.NewPubSubComms("cippa", ctx)
